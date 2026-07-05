3131eb7671fc8013f84926ff391ad946ca1a8e30
anand-2027 <anandy2027@gmail.com>
2026-07-05 14:23:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,35 @@
-const CACHE_NAME = 'anand-info-v1';
+const CACHE_NAME = 'anand-info-v2';
 const urlsToCache = [
-  '/up-edistrict-tool/index.html',
-  '/up-edistrict-tool/edistrict-resizer.html',
-  '/up-edistrict-tool/aadhaar-cutter.html',
-  '/up-edistrict-tool/form-filler.html',
-  '/up-edistrict-tool/print-optimizer.html'
+  './index.html',
+  './edistrict-resizer.html',
+  './aadhaar-cutter.html',
+  './form-filler.html',
+  './print-optimizer.html',
+  './style.css',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) { return response; }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => caches.match('./index.html'));
+    })
   );
 });
